eba8dd35b2f22f7d1a878fa71ed05303bb4f2768
Sahan <sapeirisw@gmail.com>
2014-11-30 11:36:24 -0800

    added some custom styling wrote tests for password validation added resources user to routes created partial _error_messages created a user spec model wrote flash message for successful user signup have to change the css so that when an error occurs only the text box is highlighted and not the text as well Gravatar works perfect!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, :type => :model do
 
   before :each do
     User.delete_all
-    @user = User.new(name:"Sahan Peiris", email: "sapeirisw@gmail.com",
-            ripple_account: "sapeiris")
+    @user = User.new(name:"Sahan Peiris", password: "sahan1234", password_confirmation: "sahan1234",
+                     email: "sapeirisw@gmail.com", ripple_account: "sapeiris")
   end
 
   it "should be a valid user" do
@@ -31,5 +31,43 @@ RSpec.describe User, :type => :model do
     @user.email = "a" * 256
     expect(@user).to_not be_valid
   end
-  
+
+  it "should only accept valid email addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).to be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com, @yolo.edu]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).to_not be_valid
+    end
+  end
+
+  it "should not allow users with duplicate emails" do
+    dup_user = @user.dup
+    @user.save
+    expect(dup_user).to_not be_valid
+  end
+
+  it "should not allow users with duplicate emails even if there is a case mismatch" do
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    expect(dup_user).to_not be_valid
+  end
+
+  it "should have a password of minimum length" do
+    @user.password = "a" * 5
+    @user.password_confirmation = "a" * 5
+    expect(@user).to_not be_valid
+  end
+
+
 end

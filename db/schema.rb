99efc1ce0cbfce4b2a929982123e345d66772082
Sahan <sapeirisw@gmail.com>
2014-12-09 17:02:12 -0800

    Implemented remember me

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130072442) do
+ActiveRecord::Schema.define(version: 20141210003740) do
 
   create_table "posts", force: true do |t|
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -27,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141130072442) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
